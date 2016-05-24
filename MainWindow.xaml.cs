3dee809394b6f0a65e6d3d8b8bce0585e8aa875e
Jesus Gonzalez <gkz2024@gmail.com>
2016-05-24 11:06:46 +0200

    Con los cambios realizados en el segundo día de clase con Alberto

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,13 +19,58 @@ using System.Windows.Shapes;
 namespace NCS_Formacion_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Busqueda busquedaActual;
+        private List<Busqueda> ListaBusquedas = new List<Busqueda> { };
+        private Busqueda busquedaTest = new Busqueda();
+
         public MainWindow()
         {
             InitializeComponent();
+            busquedaActual = new Busqueda();
+            this.DataContext = busquedaActual;
+            ListaBusqueda.ItemsSource = ListaBusquedas;
+            ListaBusqueda.ItemsSource = ListaBusquedas;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string ret = GetJSon();
+            resultado peliculas = JsonConvert.DeserializeObject<resultado>(ret);
+            listView.ItemsSource = peliculas.Search;
+        }
+
+        private string GetJSon()
+        {
+            string ret = string.Empty;
+            string url = busquedaActual.GetURL();
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
+            Stream resStream = resp.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            ret = reader.ReadToEnd();
+
+            return ret;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (listView.SelectedIndex > -1)
+            {
+                DetallePelicula dp = new DetallePelicula();
+                dp.pelicula = (Pelicula)listView.SelectedItem;
+                dp.Show();
+            }
+
+
         }
     }
 }
